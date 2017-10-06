47a0966cc9c43e572994c7d25410e5b7fb0cbfd9
AkhilHector <akhilhector.1@gmail.com>
2017-10-06 15:28:09 -0500

    Init boolean retrieval module for golang
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- go/modules/ir.go
+++ go/modules/ir.go
@@ -1,3 +1,19 @@
 /* This Source Code Form is subject to the terms of the MIT
  * License. If a copy of the same was not distributed with this
- * file, You can obtain one at https://github.com/AkhilHector/parsy/blob/master/LICENSE.*/
+ * file, You can obtain one at https://github.com/akhilpandey95/parsy/blob/master/LICENSE.*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Sentence struct {
+	Input string
+}
+
+// Various Information retrieval methods
+func (s Sentence) boolean_retrieval() []string {
+	return s.Input
+}
