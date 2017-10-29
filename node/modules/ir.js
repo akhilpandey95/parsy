56886508158ab8406d39f8fc12887e35cbde5b1f
AkhilHector <akhilhector.1@gmail.com>
2017-10-28 20:52:33 -0500

    Touched IR - information retrieval methods for node/modules
    Signed-off-by: AkhilHector <akhilhector.1@gmail.com>

--- node/modules/ir.js
+++ node/modules/ir.js
@@ -1,3 +1,15 @@
 /* This Source Code Form is subject to the terms of the MIT
  * License. If a copy of the same was not distributed with this
  * file, You can obtain one at https://github.com/AkhilHector/parsy/blob/master/LICENSE.*/
+
+module.exports.cosine_sim = (sentence_one, sentence_two) => {
+    return sentence_one + sentence_two
+}
+
+module.exports.ngrams = (sentence_one, sentence_two) => {
+    return sentence_one, sentence_two
+}
+
+module.exports.tfidf = (sentence_one, sentence_two) => {
+    return sentence_one, sentence_two
+}
